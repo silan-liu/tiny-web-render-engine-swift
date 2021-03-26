6f8fac3c210c14efa6e482ab00c7113c2b6147c2
silan-liu <413769707@qq.com>
2021-03-26 10:54:04 +0800

    Initial Commit

---
+++ TinyRenderEngine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinyRenderEngine
+//
+//  Created by silan on 2021/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
